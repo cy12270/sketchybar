6531e1ead900a634f017e5e3d5daf020d0e7a523
cy12270 <cy12270@gmail.com>
2026-06-20 20:34:00 +0800

    change bar colour to semi-transparent black

--- styles/colors.sh
+++ styles/colors.sh
@@ -2,6 +2,7 @@
 
 # -- Teal Scheme --
 export WHITE=0xffffffff
-export BAR_COLOR=0xff001f30
+export TRANSPARENT=0x00000000
+export BAR_COLOR=0x44000000
 export ITEM_BG_COLOR=0xff003547
 export ACCENT_COLOR=0xff2cf9ed
